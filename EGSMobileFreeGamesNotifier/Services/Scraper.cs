688941866dd763fcce3d8dec7b5ba905205a3ef5
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-19 17:17:15 +0800

    Update non-browser-automation approach to prevent errors caused by cf validations.

--- EGSMobileFreeGamesNotifier/Services/Scraper.cs
+++ EGSMobileFreeGamesNotifier/Services/Scraper.cs
@@ -1,31 +1,43 @@
 ﻿using EGSMobileFreeGamesNotifier.Strings;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace EGSMobileFreeGamesNotifier.Services {
     internal class Scraper: IDisposable {
         private readonly ILogger<Scraper> _logger;
 
-        internal HttpClient Client { get; set; } = new();
+        private readonly int MaxTries = 3;
 
-        public Scraper(ILogger<Scraper> logger) {
+		private readonly HttpClient Client;
+		private readonly CookieContainer _cookieContainer = new();
+
+		public Scraper(ILogger<Scraper> logger) {
             _logger = logger;
-            Client.DefaultRequestHeaders.UserAgent.ParseAdd(ScraperStrings.UserAgent);
+
+			var handler = new HttpClientHandler {
+				CookieContainer = _cookieContainer,
+				UseCookies = true
+			};
+
+			Client = new(handler);
+
+            Client.DefaultRequestHeaders.Add(ScraperStrings.UserAgentKey, ScraperStrings.UserAgentValue);
             Client.DefaultRequestHeaders.Add(ScraperStrings.CacheControlKey, ScraperStrings.CacheControlValue);
-			Client.DefaultRequestHeaders.Add(ScraperStrings.PragmaKey, ScraperStrings.PragmaValue);
+            Client.DefaultRequestHeaders.Add(ScraperStrings.PragmaKey, ScraperStrings.PragmaValue);
+            Client.DefaultRequestHeaders.Add(ScraperStrings.AcceptKey, ScraperStrings.AcceptValue);
+            Client.DefaultRequestHeaders.Add(ScraperStrings.AcceptLanguageKey, ScraperStrings.AcceptLanguageValue);
+			// Client.DefaultRequestHeaders.Add(ScraperStrings.CookieKey, ScraperStrings.CookieValue);
 		}
 
-        internal async Task<Tuple<string, string>> GetSource() {
+		internal async Task<Tuple<string, string>> GetSource() {
             try {
                 _logger.LogDebug(ScraperStrings.debugGetSource);
 
-                // send one dummy request first to get cloudflare validation passed
-                var resp = await Client.GetAsync(ScraperStrings.UrlAndroid);
+                var androidContent = await TryGetData(ScraperStrings.UrlAndroid, "android");
+				var iosContent = await TryGetData(ScraperStrings.UrlIOS, "ios");
 
-				resp = await Client.GetAsync(ScraperStrings.UrlAndroid);
-				var androidContent = await resp.Content.ReadAsStringAsync();
-
-				resp = await Client.GetAsync(ScraperStrings.UrlIOS);
-                var iosContent = await resp.Content.ReadAsStringAsync();
+				File.WriteAllText("Test\\android.json", androidContent);
+				File.WriteAllText("Test\\ios.json", iosContent);
 
 				_logger.LogDebug($"Done: {ScraperStrings.debugGetSource}");
                 return new (androidContent, iosContent);
@@ -37,7 +49,32 @@ namespace EGSMobileFreeGamesNotifier.Services {
             }
         }
 
-        public void Dispose() {
+		private async Task<string> TryGetData(string url, string platform) {
+            try {
+                int tryCount = 0;
+                while (++tryCount < MaxTries) {
+                    _logger.LogDebug(ScraperStrings.debugTryMessage, tryCount, platform);
+
+                    var uri = new Uri(url);
+
+					var resp = await Client.GetAsync(uri);
+                    var tmpContent = await resp.Content.ReadAsStringAsync();
+
+                    if (tmpContent.StartsWith('{')) return tmpContent;
+
+                    PrintCookies(uri);
+
+                    await Task.Delay(new Random().Next(2, 5) * 500);
+				}
+                throw new Exception(string.Format(ScraperStrings.errorGetDataFailed, platform, tryCount));
+            } catch (Exception) {
+                throw;
+            }
+		}
+
+		private void PrintCookies(Uri uri) => _cookieContainer.GetCookies(uri).ToList().ForEach(cookie => _logger.LogInformation($"Cookie: {cookie.Name} = {cookie.Value}"));
+
+		public void Dispose() {
             GC.SuppressFinalize(this);
         }
     }
--- EGSMobileFreeGamesNotifier/Strings/ScraperStrings.cs
+++ EGSMobileFreeGamesNotifier/Strings/ScraperStrings.cs
@@ -6,16 +6,26 @@
         internal static readonly string UrlIOS = $"{UrlBase}ios";
 
 		#region headers string
-		internal static readonly string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0";
+		internal static readonly string UserAgentKey = "User-Agent";
+		internal static readonly string UserAgentValue = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0";
         internal static readonly string CacheControlKey = "Cache-Control";
 		internal static readonly string CacheControlValue = "no-cache";
         internal static readonly string PragmaKey = "Pragma";
 		internal static readonly string PragmaValue = "no-cache";
+		internal static readonly string CookieKey = "Cookie";
+		internal static readonly string CookieValue = "__cf_bm=; __cflb=; _cfuvid=; cf_clearance=;";
+		internal static readonly string AcceptKey = "Accept";
+		internal static readonly string AcceptValue = "application/json";
+		internal static readonly string AcceptLanguageKey = "Accept-Language";
+		internal static readonly string AcceptLanguageValue = "en-US,en;q=0.9";
 		#endregion
 
 		#region debug strings
 		internal static readonly string debugGetSource = "Get source";
         internal static readonly string debugGetSourceWithUrl = "Getting source: {0}";
-        #endregion
-    }
+		internal static readonly string debugTryMessage = "Trying to get json data: {0} | {1}";
+
+		internal static readonly string errorGetDataFailed = "Failed to get data from {0} after {1} tries";
+		#endregion
+	}
 }
